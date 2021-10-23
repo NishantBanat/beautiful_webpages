7d1242b963811bfe162e7882feda5faae5365732
NishantBanat <83485815+NishantBanat@users.noreply.github.com>
2021-10-23 16:34:24 +0530

    Created home page.

--- src/restaurant_app/home/views.py
+++ src/restaurant_app/home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, template_name="home/home.html", context=context)
+    return render(request, template_name="home/home_page.html", context=context)
